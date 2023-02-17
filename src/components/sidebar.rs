8106b7934879640ef0b1ce5b9fbff821d30d9d0c
DavFeng1 <fenghourun@gmail.com>
2023-02-17 02:37:35 -0500

    Begin implementing custom widget

--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -2,7 +2,7 @@ use crate::app::{App, FocusElement};
 use tui::{
     backend::Backend,
     buffer::Buffer,
-    layout::{Constraint, Direction, Layout, Rect},
+    layout::{Corner, Constraint, Direction, Layout, Rect},
     style::{Color, Style},
     text::{Span, Spans},
     widgets::{Block, Borders, List, ListItem, Paragraph, Widget},
@@ -25,6 +25,7 @@ pub fn render<B: Backend>(f: &mut Frame<B>, app: &mut App, area: Rect) {
 
     let mut list_items: Vec<ListItem> = vec![];
     let mut table_list: Vec<String> = vec![];
+    let mut database_string_list: Vec<String> = vec![];
 
     for database in database_list {
         let database_name = database.name.as_str();
@@ -33,6 +34,7 @@ pub fn render<B: Backend>(f: &mut Frame<B>, app: &mut App, area: Rect) {
             table_list.push(table);
         }
 
+        database_string_list.push(String::from(database_name));
         list_items.push(ListItem::new(database_name));
     }
 
@@ -52,29 +54,73 @@ pub fn render<B: Backend>(f: &mut Frame<B>, app: &mut App, area: Rect) {
 
     // Try building the tree
 
-    let border_block1 = Block::default()
+    let border_block = Block::default()
         .title(title)
         .borders(Borders::ALL)
         .style(Style::default().fg(render_color));
 
     let items = vec![String::from("hello"), String::from("world")];
 
-    f.render_widget(CustomWidget::new(items), area);
+    f.render_widget(
+        CustomWidget::new(database_string_list).block(border_block),
+        area
+    );
 }
 
 #[derive(Debug, Clone)]
-struct CustomWidget {
+struct CustomWidget<'a> {
+    block: Option<Block<'a>>,
     items: Vec<String>,
+    start_corner: Corner,
 }
 
-impl CustomWidget {
-    pub fn new(items: Vec<String>) -> CustomWidget {
-        Self { items }
+impl<'a> CustomWidget<'a> {
+    pub fn new(items: Vec<String>) -> CustomWidget<'a> {
+        Self {
+            block: None,
+            items,
+            start_corner: Corner::TopLeft,
+        }
+    }
+
+    pub fn block(mut self, block: Block<'a>) -> CustomWidget<'a> {
+        self.block = Some(block);
+        self
     }
 }
 
-impl<'a> Widget for CustomWidget {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        println!("Not implemented");
+impl<'a> Widget for CustomWidget<'a> {
+    fn render(mut self, area: Rect, buf: &mut Buffer) {
+
+        let list_area = match self.block.take() {
+            Some(b) => {
+                let inner_area = b.inner(area);
+                b.render(area, buf);
+                inner_area
+            }
+            None => area,
+        };
+
+        for item in self.items.iter_mut()
+        {
+            let area = Rect {
+                x: 0,
+                y: 0,
+                width: list_area.width,
+                height: 40 as u16,
+            };
+
+
+            buf.set_stringn(
+                area.left() as u16,
+                area.top(),
+                item,
+                40 as usize,
+                Style::default(),
+            );
+        }
     }
 }
+
+
+
