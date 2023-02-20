fbf9cd87d04f6d88f19662e967786abb5f367b08
David Feng <fenghourun@gmail.com>
2023-02-20 16:52:23 -0500

    Table change logic -- done

--- src/widgets/database_cluster.rs
+++ src/widgets/database_cluster.rs
@@ -9,6 +9,7 @@ pub struct DatabaseCluster {
     pub tables_map: HashMap<String, Vec<String>>,
     pub current_connected_database: Option<usize>,
     pub current_focused_database: Option<usize>,
+    pub current_focused_table: Option<usize>,
 }
 
 impl DatabaseCluster {
@@ -18,10 +19,25 @@ impl DatabaseCluster {
             tables_map: HashMap::default(),
             current_connected_database: None,
             current_focused_database: None,
+            current_focused_table: None,
         }
     }
 
     pub fn next(&mut self) {
+        match self.current_connected_database {
+            Some(_current_connected_database) => self.next_table(),
+            None => self.next_database(),
+        }
+    }
+
+    pub fn prev(&mut self) {
+        match self.current_connected_database {
+            Some(_current_connected_database) => self.prev_table(),
+            None => self.prev_database(),
+        }
+    }
+
+    pub fn next_database(&mut self) {
         let next_database_index = match self.current_focused_database {
             Some(focused_db_index) => {
                 self.databases[focused_db_index].is_focused = false;
@@ -39,7 +55,7 @@ impl DatabaseCluster {
         self.databases[next_database_index].is_focused = true;
     }
 
-    pub fn prev(&mut self) {
+    pub fn prev_database(&mut self) {
         let prev_database_index = match self.current_focused_database {
             Some(focused_db_index) => {
                 self.databases[focused_db_index].is_focused = false;
@@ -55,6 +71,62 @@ impl DatabaseCluster {
 
         self.current_focused_database = Some(prev_database_index);
         self.databases[prev_database_index].is_focused = true;
+    }
+
+    pub fn next_table(&mut self) {
+        match self.current_connected_database {
+            Some(current_connnected_database) => {
+                let next_table_index = match self.current_focused_table {
+                    Some(focused_table_index) => {
+                        self.databases[current_connnected_database].tables[focused_table_index]
+                            .is_focused = false;
+
+                        let number_of_tables =
+                            self.databases[current_connnected_database].tables.len();
+
+                        if focused_table_index >= number_of_tables - 1 {
+                            0
+                        } else {
+                            focused_table_index + 1
+                        }
+                    }
+                    None => 0,
+                };
+
+                self.current_focused_table = Some(next_table_index);
+                self.databases[current_connnected_database].tables[next_table_index].is_focused =
+                    true;
+            }
+            None => (),
+        }
+    }
+
+    pub fn prev_table(&mut self) {
+        match self.current_connected_database {
+            Some(current_connnected_database) => {
+                let prev_table_index = match self.current_focused_table {
+                    Some(focused_table_index) => {
+                        self.databases[current_connnected_database].tables[focused_table_index]
+                            .is_focused = false;
+
+                        let number_of_tables =
+                            self.databases[current_connnected_database].tables.len();
+
+                        if focused_table_index <= 0 {
+                            number_of_tables - 1
+                        } else {
+                            focused_table_index - 1
+                        }
+                    }
+                    None => 0,
+                };
+
+                self.current_focused_table = Some(prev_table_index);
+                self.databases[current_connnected_database].tables[prev_table_index].is_focused =
+                    true;
+            }
+            None => (),
+        }
     }
 
     pub fn toggle_select_focused_element(&mut self) {
